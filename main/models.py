d718fb4b0a1d025ba7697719e1eb4b846549e443
unknown <owais1531997@gmail.com>
2021-04-05 07:28:03 +0400

    initial commit

--- main/models.py
+++ main/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Meals(models.Model):
+    name=models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 
-class CalorieCal(models.Model):
-    Age=models.IntegerField()
-    Weight=models.IntegerField()
-    Height=models.IntegerField()
-    Activity=models.IntegerField()
+class FoodType(models.Model):
+    name=models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+        
+
+class Food(models.Model):
+    name=models.CharField(max_length=200)
+    calories=models.IntegerField()
+    carbs=models.FloatField()
+    fats=models.FloatField()
+    protien=models.FloatField()
+    per_how_much_gram=models.IntegerField() 
+    FoodType=models.ForeignKey('FoodType',on_delete=models.CASCADE, null=True)
+    MealType=models.ForeignKey('Meals',on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.name + '-' + self.FoodType.name + '-' + self.MealType.name
+
+class Units(models.Model):
+    Ounce=models.IntegerField()
+    Grams=models.IntegerField()
+    Lb=models.IntegerField()
--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
  path('', views.home , name='home'),
- path('calories', views.CountCal),
+ path('diet', views.DietPlan, name='diet'),
 ]
